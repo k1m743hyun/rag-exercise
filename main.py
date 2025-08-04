88b6191d686295912a05c5d5b551e39b02b14295
Taehyun Kim <p_taehyun.kim@lotte.net>
2025-08-04 16:06:13 +0900

    Execute combined prompt at ChatGPT

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import os
 import asyncio
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 os.environ['OPENAI_API_KEY'] = ''
 
 
 def createPrompt(command):
-    return ChatPromptTemplate.from_template(command)
+    return PromptTemplate.from_template(command)
 
 
 if __name__ == '__main__':
@@ -22,6 +22,6 @@ if __name__ == '__main__':
         + "\n\n{language}로 번역해주세요."
     )
 
-    filled_prompt = combined_prompt.format(name="홍길동", age=30, language="영어")
-
-    print(filled_prompt)
+    llm = ChatOpenAI(model="gpt-4o-mini")
+    chain = combined_prompt | llm | StrOutputParser()
+    print(chain.invoke({"age": 30, "language": "영어", "name": "홍길동"}))
