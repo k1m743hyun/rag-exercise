d00d4674bbf87fb5e22ea1101b2b57fd04d3707f
Taehyun Kim <p_taehyun.kim@lotte.net>
2025-08-04 14:55:21 +0900

    Modify main.py

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import os
 from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
 
 os.environ['OPENAI_API_KEY'] = ''
+
+
+def createPrompt():
+    return ChatPromptTemplate.from_template(
+        "You are an expert in astronomy. Answer the question. <Question>: {input}"
+    )
+
 
 if __name__ == '__main__':
     llm = ChatOpenAI(model="gpt-4o-mini")
 
-    print(llm.invoke("지구의 자전 주기는?"))
+    #print(llm.invoke("지구의 자전 주기는?"))
+
+    chain = createPrompt() | llm
+
+    print(chain.invoke({"input": "지구의 자전 주기는?"}))
