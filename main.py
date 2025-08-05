90dd15ceaed67c30021c8cee7a8c2c625395e6df
김태현 <k1m743hyun@icloud.com>
2025-08-05 16:49:41 +0900

    Use WebBaseLoader

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
-import os
-from langchain_core.prompts import PromptTemplate
-from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.output_parsers import JsonOutputParser
-from langchain_core.pydantic_v1 import BaseModel, Field
-
-os.environ['GOOGLE_API_KEY'] = ''
-
-class CusineRecipe(BaseModel):
-    name: str = Field(description="name of a cusine")
-    recipe: str = Field(description="recipe to cook the cusine")
+from langchain_community.document_loaders import WebBaseLoader
 
 if __name__ == '__main__':
-    output_parser = JsonOutputParser(pydantic_object=CusineRecipe)
-    format_instructions = output_parser.get_format_instructions()
-    print(format_instructions)
+    url = 'https://ko.wikipedia.org/wiki/%EC%9C%84%ED%82%A4%EB%B0%B1%EA%B3%BC:%EC%A0%95%EC%B1%85%EA%B3%BC_%EC%A7%80%EC%B9%A8'
+    loader = WebBaseLoader(url)
 
-    prompt = PromptTemplate(
-        template="Answer the user query.\n{format_instructions}\n{query}\n",
-        input_variables=["query"],
-        partial_variables={"format_instructions": format_instructions},
-    )
-    print(prompt)
+    docs = loader.load()
 
-    model = ChatGoogleGenerativeAI(
-        model="gemini-1.5-flash",
-        temperature=0,
-        max_tokens=200,
-    )
-
-    chain = prompt | model | output_parser
-
-    result = chain.invoke({"query": "Let me know how to cook Bibimbap"})
-    print(result)
+    print(len(docs))
+    print(len(docs[0].page_content))
+    print(docs[0].page_content[5000:6000])
