c068684243116fa0148665f4bab374fb2906ded2
김태현 <k1m743hyun@icloud.com>
2025-08-05 10:05:03 +0900

    Use MessagePromptTemplate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 os.environ['OPENAI_API_KEY'] = ''
@@ -8,8 +8,8 @@ os.environ['OPENAI_API_KEY'] = ''
 
 def createPrompt():
     return ChatPromptTemplate.from_messages([
-        ("system", "이 시스템은 천문학 질문에 답변할 수 있습니다."),
-        ("user", "{user_input}")
+        SystemMessagePromptTemplate.from_template("이 시스템은 천문학 질문에 답변할 수 있습니다."),
+        HumanMessagePromptTemplate.from_template("{user_input}")
     ])
 
 
@@ -17,6 +17,7 @@ if __name__ == '__main__':
     chat_prompt = createPrompt()
 
     #messages = chat_prompt.format_messages(user_input="태양계에서 가장 큰 행성은 무엇인가요?")
+    #print(messages)
 
     llm = ChatOpenAI(model="gpt-4o-mini")
     chain = chat_prompt | llm | StrOutputParser()
