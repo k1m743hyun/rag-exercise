b04aac63ee9787a77e782148a35728f1d61574ca
김태현 <k1m743hyun@icloud.com>
2025-08-05 15:47:34 +0900

    Use Claude

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
 import os
+from langchain_anthropic import ChatAnthropic
 
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_google_genai import ChatGoogleGenerativeAI
-
-os.environ['GOOGLE_API_KEY'] = ''
+os.environ['ANTHROPIC_API_KEY'] = ''
 
 if __name__ == '__main__':
-    prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", "이 시스템은 천문학 질문에 답변할 수 있습니다."),
-            ("user", "{user_input}"),
-        ]
+    model = ChatAnthropic(
+        model="claude-3-haiku-2024",
+        temperature=0,
+        max_tokens=200,
     )
 
-    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", max_tokens=100)
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "LLM은 어떤 원리로 작동하나요? 100자 이내로 설명해주세요."}
+    ]
 
-    messages = prompt.format_messages(user_input="태양계에서 가장 큰 행성은 무엇인가요?")
-
-    before_answer = model.invoke(messages)
-    print(before_answer)
-
-    chain = prompt | model.bind(max_tokens=10)
-
-    after_answer = chain.invoke({"user_input": "태양계에서 가장 큰 행성은 무엇인가요?"})
-    print(after_answer)
+    result = model.invoke(messages)
+    print(result)
