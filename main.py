dbbf6227e1092af10c98370191a599d8a4eb0f15
김태현 <k1m743hyun@icloud.com>
2025-08-05 16:16:05 +0900

    Use CommaSeparatedListOutputParser

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import os
+from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import CommaSeparatedListOutputParser
 
 os.environ['GOOGLE_API_KEY'] = ''
 
 if __name__ == '__main__':
+    output_parser = CommaSeparatedListOutputParser()
+    format_instructions = output_parser.get_format_instructions()
+    #print(format_instructions)
+
+    prompt = PromptTemplate(
+        template="List five {subject}.\n{format_instructions}",
+        input_variables=["subject"],
+        partial_variables={"format_instructions": format_instructions},
+    )
+
     model = ChatGoogleGenerativeAI(
         model="gemini-1.5-flash",
         temperature=0,
         max_tokens=200,
     )
 
-    messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "LLM은 어떤 원리로 작동하나요? 100자 이내로 설명해주세요."}
-    ]
+    chain = prompt | model | output_parser
 
-    result = model.invoke(messages)
+    result = chain.invoke({"subject": "popular Korean cusine"})
     print(result)
