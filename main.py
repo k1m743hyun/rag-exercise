bec97aa9895cf8c7e73017607f5043bbd7401eea
김태현 <k1m743hyun@icloud.com>
2025-08-05 16:28:29 +0900

    Use JsonOutputParser

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 import os
 from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.output_parsers import CommaSeparatedListOutputParser
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.pydantic_v1 import BaseModel, Field
 
 os.environ['GOOGLE_API_KEY'] = ''
 
+class CusineRecipe(BaseModel):
+    name: str = Field(description="name of a cusine")
+    recipe: str = Field(description="recipe to cook the cusine")
+
 if __name__ == '__main__':
-    output_parser = CommaSeparatedListOutputParser()
+    output_parser = JsonOutputParser(pydantic_object=CusineRecipe)
     format_instructions = output_parser.get_format_instructions()
-    #print(format_instructions)
+    print(format_instructions)
 
     prompt = PromptTemplate(
-        template="List five {subject}.\n{format_instructions}",
-        input_variables=["subject"],
+        template="Answer the user query.\n{format_instructions}\n{query}\n",
+        input_variables=["query"],
         partial_variables={"format_instructions": format_instructions},
     )
+    print(prompt)
 
     model = ChatGoogleGenerativeAI(
         model="gemini-1.5-flash",
@@ -24,5 +30,5 @@ if __name__ == '__main__':
 
     chain = prompt | model | output_parser
 
-    result = chain.invoke({"subject": "popular Korean cusine"})
+    result = chain.invoke({"query": "Let me know how to cook Bibimbap"})
     print(result)
