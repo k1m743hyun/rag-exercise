779c5bce60ea88fc89abd8d6c6d592a1a8b691f5
김태현 <k1m743hyun@icloud.com>
2025-08-05 15:28:15 +0900

    input params when the chat model created

--- main.py
+++ main.py
@@ -5,19 +5,22 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 os.environ['GOOGLE_API_KEY'] = ''
 
 if __name__ == '__main__':
-    chat_prompt = ChatPromptTemplate.from_messages(
-        [
-            ("system", "이 시스템은 여행 전문가입니다."),
-            ("user", "{user_input}"),
-        ]
-    )
+    params = {
+        "temperature": 0.7,
+        "max_tokens": 100
+    }
 
-    chat_model = ChatGoogleGenerativeAI(
+    kwargs = {
+        "frequency_penalty": 0.5,
+        "presence_penalty": 0.5,
+        "stop": ["\n"]
+    }
+
+    model = ChatGoogleGenerativeAI(
         model="gemini-1.5-flash",
-        temperature=0,
-        max_output_tokens=200
+        **params,
+        model_kwargs=kwargs
     )
 
-    chain = chat_prompt | chat_model
-
-    print(chain.invoke({"user_input": "안녕하세요? 한국의 대표적인 관광지 3군대를 추천해주세요."}))
+    question = "태양계에서 가장 큰 행성은 무엇인가요?"
+    print(model.invoke(input=question))
