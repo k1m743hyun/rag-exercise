6edf5204a8ca9a105e33fe5264f1ed4cda5b9503
김태현 <k1m743hyun@icloud.com>
2025-08-05 15:39:09 +0900

    input params when the chat model binding

--- main.py
+++ main.py
@@ -1,33 +1,26 @@
 import os
+
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 os.environ['GOOGLE_API_KEY'] = ''
 
 if __name__ == '__main__':
-    params = {
-        "temperature": 0.7,
-        "max_tokens": 100
-    }
-
-    kwargs = {
-        "frequency_penalty": 0.5,
-        "presence_penalty": 0.5,
-        "stop": ["\n"]
-    }
-
-    model = ChatGoogleGenerativeAI(
-        model="gemini-1.5-flash",
-        **params,
-        model_kwargs=kwargs
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "이 시스템은 천문학 질문에 답변할 수 있습니다."),
+            ("user", "{user_input}"),
+        ]
     )
 
-    question = "태양계에서 가장 큰 행성은 무엇인가요?"
-    #print(model.invoke(input=question))
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", max_tokens=100)
 
-    new_params = {
-        "temperature": 0.7,
-        "max_tokens": 10,
-    }
+    messages = prompt.format_messages(user_input="태양계에서 가장 큰 행성은 무엇인가요?")
 
-    print(model.invoke(input=question, **new_params))
+    before_answer = model.invoke(messages)
+    print(before_answer)
+
+    chain = prompt | model.bind(max_tokens=10)
+
+    after_answer = chain.invoke({"user_input": "태양계에서 가장 큰 행성은 무엇인가요?"})
+    print(after_answer)
