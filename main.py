431bbef5b07f5fec14d17dd2a0cf17c2293a9279
김태현 <k1m743hyun@icloud.com>
2025-08-05 15:07:55 +0900

    Use LLM

--- main.py
+++ main.py
@@ -1,33 +1,14 @@
 import os
-from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
-from langchain_chroma import Chroma
-from langchain_core.prompts import PromptTemplate
-from langchain_core.example_selectors import SemanticSimilarityExampleSelector
-from openai import embeddings
-from datetime import datetime
+from langchain_google_genai import GoogleGenerativeAI
 
 #os.environ['OPENAI_API_KEY'] = ''
 os.environ['GOOGLE_API_KEY'] = ''
 
-def get_current_season():
-    month = datetime.now().month
-    if 3 <= month <= 5:
-        return "봄"
-    elif 6 <= month <= 8:
-        return "여름"
-    elif 9 <= month <= 11:
-        return "가을"
-    else:
-        return "겨울"
-
-def createPromptTemplate(message):
-    return PromptTemplate.from_template(message)
-
 if __name__ == '__main__':
-    prompt = PromptTemplate(
-        template="{season}에 일어나는 대표적인 지구과학 현상은 {phenomenon}입니다.",
-        input_variables=["phenomenon"],
-        partial_variables={"season": get_current_season}
+    llm = GoogleGenerativeAI(
+        model="gemini-1.5-flash",
+        temperature=0,
+        max_output_tokens=200
     )
 
-    print(prompt.format(phenomenon="꽃가루 증가"))
+    print(llm.invoke("한국의 대표적인 관광지 3군대를 추천해주세요."))
