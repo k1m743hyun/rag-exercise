d01e6afa85b3b7aa0b63728aa5c4914fb9911a68
김태현 <k1m743hyun@icloud.com>
2025-08-05 13:58:16 +0900

    Use Few-shot

--- main.py
+++ main.py
@@ -1,24 +1,67 @@
 import os
-from langchain_openai import ChatOpenAI
-from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+from langchain_core.prompts import PromptTemplate, FewShotPromptTemplate
+from langchain_core.example_selectors import SemanticSimilarityExampleSelector
 
 os.environ['OPENAI_API_KEY'] = ''
 
 
-def createPrompt():
-    return ChatPromptTemplate.from_messages([
-        SystemMessagePromptTemplate.from_template("이 시스템은 천문학 질문에 답변할 수 있습니다."),
-        HumanMessagePromptTemplate.from_template("{user_input}")
-    ])
+def createPromptTemplate():
+    return PromptTemplate.from_template("질문: {question}\n{answer}")
 
 
 if __name__ == '__main__':
-    chat_prompt = createPrompt()
 
-    #messages = chat_prompt.format_messages(user_input="태양계에서 가장 큰 행성은 무엇인가요?")
-    #print(messages)
+    example_prompt = createPromptTemplate()
 
-    llm = ChatOpenAI(model="gpt-4o-mini")
-    chain = chat_prompt | llm | StrOutputParser()
-    print(chain.invoke({"user_input": "태양계에서 가장 큰 행성은 무엇인가요?"}))
+    examples = [
+        {
+            "question": "지구의 대기 중 가장 많은 비율을 차지하는 기체는 무엇인가요?",
+            "answer": "지구 대기의 약 78%를 차지하는 질소입니다."
+        },
+        {
+            "question": "광합성에 필요한 주요 요소들은 무엇인가요?",
+            "answer": "광합성에 필요한 주요 요소는 빛, 이산화탄소, 물입니다."
+        },
+        {
+            "question": "피타고라스 정리를 설명해주세요.",
+            "answer": "피타고라스 정리는 직각삼각형에서 빗변의 제곱이 다른 두 변의 제곱의 합과 같다는 것입니다."
+        },
+        {
+            "question": "지구의 자전 주기는 얼마인가요?",
+            "answer": "지구의 자전 주기는 약 24시간(정확히는 23시간 56분 4초)입니다."
+        },
+        {
+            "question": "DNA의 기본 구조를 간단히 설명해주세요.",
+            "answer": "DNA는 두 개의 폴리뉴클레오티드 사슬이 이중 나선 구조를 이루고 있습니다."
+        },
+        {
+            "question": "원주율(π)의 정의는 무엇인가요?",
+            "answer": "원주율(π)은 원의 지름에 대한 원의 둘레의 비율입니다."
+        }
+    ]
+
+    prompt = FewShotPromptTemplate(
+        examples=examples,
+        example_prompt=example_prompt,
+        suffix="질문: {input}",
+        input_variables=["input"]
+    )
+
+    #print(prompt.invoke({"input": "화성의 표면이 붉은 이유는 무엇인가요?"}).to_string())
+
+    example_selector = SemanticSimilarityExampleSelector.from_examples(
+        examples,
+        OpenAIEmbeddings(),
+        Chroma,
+        k=1
+    )
+
+    question = "화성의 표면이 붉은 이유는 무엇인가요?"
+    selected_examples = example_selector.select_examples({"question": question})
+    print(f"입력과 가장 유사한 예제: {question}")
+    for example in selected_examples:
+        print("\n")
+        for k, v in example.items():
+            print(f"{k}: {v}")
