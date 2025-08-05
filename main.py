f993f51929a2f9739f4dffddf3724a43d43f9923
김태현 <k1m743hyun@icloud.com>
2025-08-05 16:51:55 +0900

    Use RecursiveCharacterTextSplitter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from langchain_community.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 if __name__ == '__main__':
     url = 'https://ko.wikipedia.org/wiki/%EC%9C%84%ED%82%A4%EB%B0%B1%EA%B3%BC:%EC%A0%95%EC%B1%85%EA%B3%BC_%EC%A7%80%EC%B9%A8'
@@ -6,6 +7,12 @@ if __name__ == '__main__':
 
     docs = loader.load()
 
-    print(len(docs))
-    print(len(docs[0].page_content))
-    print(docs[0].page_content[5000:6000])
+    #print(len(docs))
+    #print(len(docs[0].page_content))
+    #print(docs[0].page_content[5000:6000])
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    splits = text_splitter.split_documents(docs)
+
+    print(len(splits))
+    print(splits[10])
