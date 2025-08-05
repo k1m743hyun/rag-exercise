86c59e035bc718cf90486bb02d2d449e96193005
김태현 <k1m743hyun@icloud.com>
2025-08-05 15:12:19 +0900

    Use ChatModel

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 import os
-from langchain_google_genai import GoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
 
-#os.environ['OPENAI_API_KEY'] = ''
 os.environ['GOOGLE_API_KEY'] = ''
 
 if __name__ == '__main__':
-    llm = GoogleGenerativeAI(
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "이 시스템은 여행 전문가입니다."),
+            ("user", "{user_input}"),
+        ]
+    )
+
+    chat_model = ChatGoogleGenerativeAI(
         model="gemini-1.5-flash",
         temperature=0,
         max_output_tokens=200
     )
 
-    print(llm.invoke("한국의 대표적인 관광지 3군대를 추천해주세요."))
+    chain = chat_prompt | chat_model
+
+    print(chain.invoke({"user_input": "안녕하세요? 한국의 대표적인 관광지 3군대를 추천해주세요."}))
