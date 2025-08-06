4fe51de508720edb1574bae6616e2f39dcde7c2d
김태현 <k1m743hyun@icloud.com>
2025-08-06 11:29:49 +0900

    Use PyMuPDFLoader to get pdf files

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from langchain_community.document_loaders import UnstructuredPDFLoader
+from langchain_community.document_loaders import PyMuPDFLoader
 
 if __name__ == '__main__':
     pdf_filepath = 'data/000660_SK_2023.pdf'
-    loader = UnstructuredPDFLoader(pdf_filepath, mode='elements')
+    loader = PyMuPDFLoader(pdf_filepath)
     pages = loader.load()
     print(len(pages))
-    print(pages[100])
+    print(pages[0].page_content)
+    print(pages[0].metadata)
