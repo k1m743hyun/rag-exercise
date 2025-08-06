f6dd1d798d38133b1b170b34ad8d6b760c9abd1f
김태현 <k1m743hyun@icloud.com>
2025-08-06 11:11:43 +0900

    Use UnstructuredPDFLoader to get pdf files

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import UnstructuredPDFLoader
 
 if __name__ == '__main__':
-    pdf_filepath = './data/000660_SK_2023.pdf'
-    loader = PyPDFLoader(pdf_filepath)
+    pdf_filepath = 'data/000660_SK_2023.pdf'
+    loader = UnstructuredPDFLoader(pdf_filepath)
     pages = loader.load()
     print(len(pages))
-    print(pages[10])
+    print(pages[0].page_content[:1000])
+    print(pages[0].metadata)
