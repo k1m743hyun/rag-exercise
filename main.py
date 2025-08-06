fe60311a174c039462df602d19957f70983d81fb
김태현 <k1m743hyun@icloud.com>
2025-08-06 10:33:37 +0900

    Use PyPDFLoader to get pdf files

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from langchain_community.document_loaders.csv_loader import CSVLoader
+from langchain_community.document_loaders import PyPDFLoader
 
 if __name__ == '__main__':
-    loader = CSVLoader(file_path='./data/한국주택금융공사_주택금융관련_지수_20160101.csv', encoding='cp949',
-                       csv_args={
-                           'delimiter': '\n'
-                       })
-    data = loader.load()
-    print(len(data))
-    print(data[0])
+    pdf_filepath = './data/000660_SK_2023.pdf'
+    loader = PyPDFLoader(pdf_filepath)
+    pages = loader.load()
+    print(len(pages))
+    print(pages[10])
