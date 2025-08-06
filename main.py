72341d14902ad44517084b524544014840484dbb
김태현 <k1m743hyun@icloud.com>
2025-08-06 11:52:03 +0900

    Use CharacterTextSplitter

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
 
 if __name__ == '__main__':
-    loader = PyPDFDirectoryLoader('./data')
+    loader = TextLoader('./data/history.txt')
     data = loader.load()
-    print(len(data))
-    print(data[0])
-    print(data[-1])
+    #print(len(data[0].page_content))
+    #print(data[0].page_content)
+
+    text_splitter = CharacterTextSplitter(
+        separator='\n',
+        chunk_size=500,
+        chunk_overlap=100,
+        length_function=len,
+    )
+
+    texts = text_splitter.split_text(data[0].page_content)
+    print(len(texts))
+    print(len(texts[0]), len(texts[1]), len(texts[2]))
+    print(texts[0])
