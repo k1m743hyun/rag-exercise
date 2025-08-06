98e06fd75cc69a3ad09506d2d3f8d50de5435969
김태현 <k1m743hyun@icloud.com>
2025-08-06 10:20:15 +0900

    Use CSVLoader with csv_args option to get csv files

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from langchain_community.document_loaders.csv_loader import CSVLoader
 
 if __name__ == '__main__':
-    loader = CSVLoader(file_path='./data/한국주택금융공사_주택금융관련_지수_20160101.csv', encoding='cp949', source_column='연도')
+    loader = CSVLoader(file_path='./data/한국주택금융공사_주택금융관련_지수_20160101.csv', encoding='cp949',
+                       csv_args={
+                           'delimiter': '\n'
+                       })
     data = loader.load()
     print(len(data))
     print(data[0])
