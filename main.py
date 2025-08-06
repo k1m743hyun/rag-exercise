6d30e22d41a259d54fb839156317320ded97e90c
김태현 <k1m743hyun@icloud.com>
2025-08-06 15:50:46 +0900

    Use Maximum marginal relevance Search with Chroma

--- main.py
+++ main.py
@@ -2,39 +2,43 @@ import os
 from numpy import dot
 from numpy.linalg import norm
 from langchain_community.vectorstores import Chroma
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 os.environ['GOOGLE_API_KEY'] = ''
 
-def cos_sim(A, B):
-    return dot(A, B) / (norm(A) * norm(B))
-
 if __name__ == '__main__':
-    loader = TextLoader('data/history.txt')
+    loader = PyMuPDFLoader('data/323410_카카오뱅크_2023.pdf')
     data = loader.load()
 
     text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
-        chunk_size=250,
-        chunk_overlap=50,
+        chunk_size=1000,
+        chunk_overlap=200,
         encoding_name='cl100k_base'
     )
 
-    texts = text_splitter.split_text(data[0].page_content)
-    #print(texts[0])
+    documents = text_splitter.split_documents(data)
+    #print(len(documents))
 
     embedding_model = GoogleGenerativeAIEmbeddings(model='models/embedding-001')
 
-    db = Chroma.from_texts(
-        texts,
+    db = Chroma.from_documents(
+        documents,
         embedding_model,
-        collection_name='history',
+        collection_name='esg',
         persist_directory='./db/chromadb',
         collection_metadata={'hnsw:space': 'cosine'},
     )
     #print(db)
 
-    query = '누가 한글을 창제했나요?'
+    query = '카카오뱅크의 환경목표와 세부추진내용을 알려줘?'
     docs = db.similarity_search(query)
-    print(docs[0].page_content)
+    #print(len(docs))
+    #print(docs[0].page_content)
+    #print(docs[-1].page_content)
+
+    mmr_docs =db.max_marginal_relevance_search(query, k=4, fetch_k=10)
+    #print(len(mmr_docs))
+    #print(mmr_docs[0].page_content)
+    print(mmr_docs[-1].page_content)
