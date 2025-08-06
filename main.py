7ab0e3d634811fdc619ec93b713589b398a0ccf6
김태현 <k1m743hyun@icloud.com>
2025-08-06 10:19:17 +0900

    Use CSVLoader with source_column option to get csv files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from langchain_community.document_loaders.csv_loader import CSVLoader
 
 if __name__ == '__main__':
-    loader = CSVLoader(file_path='./data/한국주택금융공사_주택금융관련_지수_20160101.csv', encoding='cp949')
+    loader = CSVLoader(file_path='./data/한국주택금융공사_주택금융관련_지수_20160101.csv', encoding='cp949', source_column='연도')
     data = loader.load()
     print(len(data))
     print(data[0])
