0ce9b72b9650fd384f95164944544fde9506a413
김태현 <k1m743hyun@icloud.com>
2025-08-06 11:21:18 +0900

    Use UnstructuredPDFLoader with mode options to get pdf files

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from langchain_community.document_loaders import UnstructuredPDFLoader
 
 if __name__ == '__main__':
     pdf_filepath = 'data/000660_SK_2023.pdf'
-    loader = UnstructuredPDFLoader(pdf_filepath)
+    loader = UnstructuredPDFLoader(pdf_filepath, mode='elements')
     pages = loader.load()
     print(len(pages))
-    print(pages[0].page_content[:1000])
-    print(pages[0].metadata)
+    print(pages[100])
